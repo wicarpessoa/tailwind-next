e8a53e6f529149837471a307b84495f4265f89dc
Wicarpessoa <wicarpp@gmail.com>
2023-09-23 08:48:56 -0300

    feat tabs just done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from 'next/image'
+import { SettingsTab } from './components/SettingsTabs'
 
 export default function Home() {
-  return <h1 className="">Hello tailwind!</h1>
+  return (
+    <>
+      <h1 className="txt-zinc-900 text-3xl font-medium">Settings</h1>
+      <SettingsTab />
+    </>
+  )
 }
