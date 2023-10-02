3174cf4678fe64c4c3187d996648561aa99d38f3
Wicarpessoa <wicarpp@gmail.com>
2023-10-01 21:44:57 -0300

    feat component sidebar responsiveness

--- src/app/components/sidebar/index.tsx
+++ src/app/components/sidebar/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   BarChart,
   CheckSquare,
@@ -5,6 +7,7 @@ import {
   Flag,
   Home,
   LifeBuoy,
+  Menu,
   Search,
   SquareStack,
   Users,
@@ -13,35 +16,49 @@ import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import * as Collapsible from '@radix-ui/react-collapsible'
 import * as Input from '../Input'
+import { Button } from '../Button'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
-      <Logo />
-      <Input.Root>
-        <Input.Prefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </Input.Prefix>
-        <Input.Control placeholder="Search" />
-      </Input.Root>
-      <nav className="space-y-0.5">
-        <NavItem icon={Home} title="Home" />
-        <NavItem icon={BarChart} title="Dashboard" />
-        <NavItem icon={SquareStack} title="Projects" />
-        <NavItem icon={CheckSquare} title="Tasks" />
-        <NavItem icon={Flag} title="Reporting" />
-        <NavItem icon={Users} title="Users" />
-      </nav>
-      <div className=" mt-auto flex flex-col gap-6 ">
-        <nav className="space-y-0.5">
-          <NavItem icon={LifeBuoy} title="Support" />
-          <NavItem icon={Cog} title="Settings" />
-        </nav>
-        <UsedSpaceWidget />
-        <div className="h-px bg-zinc-200 " />
-        <Profile />
+    <Collapsible.Root className="fixed left-0 right-0  top-0 z-20 flex  flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0  lg:right-auto   lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 ">
+      <div className="flex items-center justify-between ">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="h-6 w-6 " />
+          </Button>
+        </Collapsible.Trigger>
       </div>
-    </aside>
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <Input.Root>
+          <Input.Prefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </Input.Prefix>
+          <Input.Control placeholder="Search" />
+        </Input.Root>
+        <nav className="space-y-0.5">
+          <NavItem icon={Home} title="Home" />
+          <NavItem icon={BarChart} title="Dashboard" />
+          <NavItem icon={SquareStack} title="Projects" />
+          <NavItem icon={CheckSquare} title="Tasks" />
+          <NavItem icon={Flag} title="Reporting" />
+          <NavItem icon={Users} title="Users" />
+        </nav>
+        <div className=" mt-auto flex flex-col gap-6 ">
+          <nav className="space-y-0.5">
+            <NavItem icon={LifeBuoy} title="Support" />
+            <NavItem icon={Cog} title="Settings" />
+          </nav>
+          <UsedSpaceWidget />
+          <div className="h-px bg-zinc-200 " />
+          <Profile />
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="grid h-screen min-h-screen grid-cols-app ">
           <Sidebar />
-          <main className="px-8 pb-12 pt-8">{children}</main>
+          <main className="px-4 pb-12 pt-24 lg:col-start-2">{children}</main>
         </div>
       </body>
     </html>
